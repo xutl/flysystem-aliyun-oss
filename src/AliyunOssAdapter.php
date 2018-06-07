612fb123a57ca87518d33a39f097ae0ef60bec54
Xu Tongle <xutongle@gmail.com>
2018-06-07 09:45:03 +0800

    init

---
+++ src/AliyunOssAdapter.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * @link http://www.tintsoft.com/
+ * @copyright Copyright (c) 2012 TintSoft Technology Co. Ltd.
+ * @license http://www.tintsoft.com/license/
+ */
+
+namespace XuTL\Flysystem\AliyunOss;
+
+use Storage;
+use Illuminate\Support\ServiceProvider;
+
+
+class AliyunOssAdapter extends ServiceProvider
+{
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Storage::extend('oss', function ($app, $config) {
+            $accessId = $config['access_id'];
+            $accessKey = $config['access_key'];
+            $endPoint = $config['endpoint'];
+            $bucket = $config['bucket'];
+            $prefix = null;
+            if (isset($config['prefix'])) {
+                $prefix = $config['prefix'];
+            }
+            $client = new OssClient($accessId, $accessKey, $endPoint);
+            $adapter = new AliyunOssAdapter($client, $bucket, $prefix);
+            $filesystem = new Filesystem($adapter);
+            $filesystem->addPlugin(new PutFile());
+            $filesystem->addPlugin(new SignedDownloadUrl());
+            return $filesystem;
+        });
+    }
+
+    
+    /**
+     * Register bindings in the container.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
