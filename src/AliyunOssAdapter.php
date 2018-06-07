0ff56b2be71b9e59fb73933ec793281dad98bb97
Xu Tongle <xutongle@gmail.com>
2018-06-07 10:53:23 +0800

    服务提供者

--- src/AliyunOssAdapter.php
+++ src/AliyunOssAdapter.php
@@ -7,45 +7,13 @@
 
 namespace XuTL\Flysystem\AliyunOss;
 
-use Storage;
 use League\Flysystem\Adapter\AbstractAdapter;
 
-
+/**
+ * 阿里云适配器
+ * @package XuTL\Flysystem\AliyunOss
+ */
 class AliyunOssAdapter extends AbstractAdapter
 {
-    /**
-     * Perform post-registration booting of services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        Storage::extend('oss', function ($app, $config) {
-            $accessId = $config['access_id'];
-            $accessKey = $config['access_key'];
-            $endPoint = $config['endpoint'];
-            $bucket = $config['bucket'];
-            $prefix = null;
-            if (isset($config['prefix'])) {
-                $prefix = $config['prefix'];
-            }
-            $client = new OssClient($accessId, $accessKey, $endPoint);
-            $adapter = new AliyunOssAdapter($client, $bucket, $prefix);
-            $filesystem = new Filesystem($adapter);
-            $filesystem->addPlugin(new PutFile());
-            $filesystem->addPlugin(new SignedDownloadUrl());
-            return $filesystem;
-        });
-    }
 
-
-    /**
-     * Register bindings in the container.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
 }
--- src/AliyunOssServiceProvider.php
+++ src/AliyunOssServiceProvider.php
@@ -6,8 +6,52 @@
  */
 
 namespace XuTL\Flysystem\AliyunOss;
-use Illuminate\Support\ServiceProvider;
-class AliyunOssServiceProvider  extends ServiceProvider
-{
 
+use Storage;
+use OSS\OssClient;
+use Illuminate\Support\ServiceProvider;
+use League\Flysystem\Filesystem;
+
+/**
+ * 阿里云OSS服务提供者
+ * @package XuTL\Flysystem\AliyunOss
+ */
+class AliyunOssServiceProvider extends ServiceProvider
+{
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Storage::extend('oss', function ($app, $config) {
+            $client = new OssClient(
+                $config['access_id'],
+                $config['access_key'],
+                $config['endpoint'],
+                $config['isCName'] ?? false,
+                $config['securityToken'] ?? null,
+                $config['proxy'] ?? null
+            );
+            $adapter = new AliyunOssAdapter(
+                $client,
+                $config['bucket'],
+                $config['prefix'] ?? null
+            );
+            $filesystem = new Filesystem($adapter);
+            return $filesystem;
+        });
+    }
+
+
+    /**
+     * Register bindings in the container.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
 }
